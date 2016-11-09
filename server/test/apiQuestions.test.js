262e49c41a546170111734fccaf468ca4abab6e7
Septian A Tama <sfirst.tama@gmail.com>
2016-11-09 15:28:03 +0700

    add test script for edit

--- server/test/apiQuestions.test.js
+++ server/test/apiQuestions.test.js
@@ -4,7 +4,7 @@ const chaiHttp = require('chai-http');
 const slug = require('slug');
 chai.use(chaiHttp);
 const should = chai.should();
-const article = require('../models/articles');
+// const article = require('../models/questions');
 
 describe('post new question', function() {
   it('should store a new question to the database, and return the data back', function(done) {
@@ -13,7 +13,7 @@ describe('post new question', function() {
       .send({
         title: 'Tanya Dong',
         content: 'Saya boleh tanya ga?',
-        author: 'orang_tamvan'
+        author: 'orang_tamvan',
         answers: [],
         up: [],
         down: [],
@@ -29,6 +29,64 @@ describe('post new question', function() {
         res.body.author.should.equal('orang_tamvan');
         res.body.slug.should.equal('tanya-dong');
         done();
+      })
+  })
+})
+
+describe('get all questions', function() {
+  it('should return the respond json which contains the list of questions from database', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/api/question')
+      .end(function (err, res) {
+        res.should.be.json;
+        res.should.have.status(200);
+        done();
+      })
+  })
+})
+
+describe('get one question based on by slug', function() {
+  let slug = 'tanya-dong';
+  it('should return the right question based on slug', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/api/question' + slug)
+      .end(function (err, res) {
+        res.should.be.json;
+        res.should.have.status(200);
+        done();
+      })
+  })
+})
+
+describe('find by slug, get the id, then update', function() {
+  let slug = 'tanya-dong';
+  it('should return status ok 1, nModified 1, modified 1', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/api/question/'+slug)
+      .end(function (err, res) {
+        console.log(res.body._id);
+        chai.request('http://localhost:3000')
+          .put('/api/question/'+res.body._id)
+          .send({
+            title: 'Tanya Dong 123',
+            content: 'Saya boleh tanya ga? 123',
+            author: 'orang_tamvan',
+            answers: [],
+            up: [],
+            down: [],
+            slug: slug(this.title).toLowerCase(),
+            createdAt: new Date(),
+            updatedAt: new Date()
+          })
+          .end(function (err, res){
+            res.should.be.json;
+            res.should.have.status(200);
+            res.body.ok.should.equal(1);
+            res.body.nModified.should.equal(1);
+            res.body.n.should.equal(1);
+            done();
+          });
+
       })
   })
 })
