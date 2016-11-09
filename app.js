b326f9f63a0cafb97dfd651aababcc7dbd18a95b
Ahyana Rizky Pratama <ahyana.rizky@gmail.com>
2016-11-09 12:50:27 +0700

    add server configuration

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
 
-var app = express();
+const routes = require('./routes/index');
+const api = require('./routes/api');
+
+const ModelUser = require('./models/user');
+const app = express();
+
+// MONGODB AND MONGOOSE
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/stackoverflow')
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,13 +35,20 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
+
+passport.use(new LocalStrategy(ModelUser.authenticate()))
+
+// BIND PASSPORT WITH USER MODEL (PASSPORT-LOCAL-MONGOOSE)
+passport.serializeUser(ModelUser.serializeUser())
+passport.deserializeUser(ModelUser.deserializeUser())
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,23 +56,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
 
