e2392c8935843da9a50111f7c8b65c700cf9434a
Septian A Tama <sfirst.tama@gmail.com>
2016-11-09 19:03:09 +0700

    add get one user by username

--- server/test/apiUser.test.js
+++ server/test/apiUser.test.js
@@ -16,3 +16,32 @@ describe('user list', function() {
       })
   })
 })
+
+
+//Test script for get article lists
+describe('user list', function() {
+  it('should return the respond json which contains the list of users from database', function(done) {
+    chai.request('http://localhost:3000')
+      .get('/api/user')
+      .end(function (err, res) {
+        res.should.be.json;
+        res.should.have.status(200);
+        done();
+      })
+  })
+})
+
+//get single article by slug
+describe('get single user by username', function() {
+  let username = 'tamatamvan';
+  it('should return a single article based on slug', function(done) {
+    chai.request('http://localhost:3000')
+    .get('/api/user/'+username)
+    .end(function (err, res) {
+      res.should.be.json;
+      res.should.have.status(200);
+      res.body.username.should.equal('tamatamvan');
+      done();
+    })
+  })
+})
