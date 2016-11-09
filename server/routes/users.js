65d607eded159ccaad88190c3ac7e04871dc3df7
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-09 15:34:32 +0700

    create router for user

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controllers/controller.api.users')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/seed', userController.seedUser)
+router.get('/', userController.getUser)
+router.get('/:userId', userController.getUserByUserId)
+router.post('/register', userController.localRegisterUser)
+router.post('/login', userController.loginUser)
+router.delete('/', userController.deleteAllUsers)
+router.delete('/:userId', userController.deleteUserByUserId)
+router.put('/:userId', userController.updateUser)
 
 module.exports = router;
