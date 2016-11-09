7f15647a5adfc8d3102f632ab62899a329a61dba
broerjuang <juangwiantoro@gmail.com>
2016-11-09 12:55:54 +0700

    adding user schema

---
+++ models/user.server.model.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+require('mongoose-type-email')
+
+const userSchema = new Schema({
+  user_id : {
+    type : Number,
+    unique : true,
+    required : true
+  },
+  username : {
+    type : String,
+    unique : true,
+    required : true
+  },
+  name : {
+    type : String,
+    required : true
+  },
+  email : {
+    type: mongoose.SchemaTypes.Email
+    required :true,
+    unique: true
+  },
+  password : {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('User', userSchema)
