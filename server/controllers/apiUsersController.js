55bfb329e1e512828418cecf25fce4ace40dd935
Septian A Tama <sfirst.tama@gmail.com>
2016-11-09 20:01:00 +0700

    controller template/skeleton for apiUsers and auth controller

---
+++ server/controllers/apiUsersController.js
@@ -0,0 +1,25 @@
+const Users = require('../models/Users');
+const slug = require('slug');
+
+let getAll = (req, res, next) => {
+
+}
+
+let getOneByUsername = (req, res, next) => {
+
+}
+
+let updateById = (req, res, next) => {
+
+}
+
+let deleteById = (req, res, next) => {
+
+}
+
+module.exports = {
+  getAll: getAll,
+  getOneByUsername: getOneByUsername,
+  updateById: updateById,
+  deleteById
+}
---
+++ server/controllers/authController.js
@@ -0,0 +1,15 @@
+const Users = require('../models/Users');
+const slug = require('slug');
+
+let register = (req, res, next) => {
+
+}
+
+let login = (req, res, next) => {
+
+}
+
+module.exports = {
+  register: register,
+  login: login
+}
