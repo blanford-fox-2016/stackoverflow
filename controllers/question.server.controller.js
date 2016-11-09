82067728e2988b45bb6e683ebcbb4d6cb8c037f1
broerjuang <juangwiantoro@gmail.com>
2016-11-09 17:37:47 +0700

    cleanup question controller

--- controllers/question.server.controller.js
+++ controllers/question.server.controller.js
@@ -4,7 +4,8 @@ const Question = require('../models/question.server.model')
 
 module.exports = {
   list: (req, res) => {
-    Question.find()
+    Question
+      .find()
       .then(questions => res.send(questions))
       .catch(err => res.send(err))
   },
@@ -19,7 +20,8 @@ module.exports = {
       //comment: ?,
       //votes: ?
     }
-    Question.create(questionData)
+    Question
+      .create(questionData)
       .then(question => res.send(question))
       .catch(err => res.send(err))
   },
@@ -39,21 +41,24 @@ module.exports = {
       new : true,
       upsert : true
     }
-    Question.findOneAndUpdate(selection, updateData, option)
+    Question
+      .findOneAndUpdate(selection, updateData, option)
       .then(question => res.json(question))
       .catch(err => res.json(err))
   },
 
   //Delete the question by its id
   delete: (req, res) => {
-    Question.findOneAndRemove({question_id : req.params.question_id})
+    Question
+      .findOneAndRemove({question_id : req.params.question_id})
       .then(question => res.json(question))
       .err(err => res.json(err))
   },
 
   // GET the question by its id
   find: (req, res) => {
-    Question.findOne({question_id : req.params.question_id})
+    Question
+      .findOne({question_id : req.params.question_id})
       .then(question => res.json(question))
       .catch(err => res.json(err))
   }
