d1fc3bf43f0a2aab5a502c9e0989056930e8bb16
Septian A Tama <sfirst.tama@gmail.com>
2016-11-10 11:45:38 +0700

    add getAll users method

--- server/controllers/apiUsersController.js
+++ server/controllers/apiUsersController.js
@@ -1,7 +1,13 @@
 const Users = require('../models/Users');
 
 let getAll = (req, res, next) => {
-
+  Users.find({}, (err, users) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(users);
+    }
+  })
 }
 
 let getOneByUsername = (req, res, next) => {
