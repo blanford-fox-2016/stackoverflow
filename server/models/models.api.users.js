a836aaf3cb2284db84c278ff89af335dce6bdf49
Ken Duigraha utra <kenduigraha@yahoo.com>
2016-11-17 02:09:03 +0700

    updated user's schema

--- server/models/models.api.users.js
+++ server/models/models.api.users.js
@@ -7,17 +7,20 @@ const passportLocalMongoose = require('passport-local-mongoose')
 const Schema = mongoose.Schema
 
 let UsersSchema = new Schema({
-  userId  : Number,
   username : {
     type      : String,
     required  : true
   },
   password : {
     type      : String,
-    requires  : true,
+    required  : true,
     lowercase : true,
     trim      : true
-  }
+  },
+  up_vote: Boolean,
+  down_vote: Boolean
+},{
+  timestamps: true
 })
 
 UsersSchema.plugin(passportLocalMongoose)
